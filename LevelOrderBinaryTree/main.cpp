bd1f8826b2ba51148f6dd5ee4d50b08e63ce4fa7
陈宇峰 <yufengcode@gmail.com>
2016-10-01 15:56:58 +0800

    创建树与先序遍历。

--- LevelOrderBinaryTree/main.cpp
+++ LevelOrderBinaryTree/main.cpp
@@ -8,8 +8,78 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+/**
+ *  层序遍历二叉树
+ */
+
+typedef struct TreeNode{
+    int element;
+    struct TreeNode *lChild;
+    struct TreeNode *rChild;
+}Node;
+
+void CreateBinaryTree(Node **root)
+{
+    int num;
+    cin >> num;
+
+    if (num == -1)
+    {
+        return;
+    }
+
+    *root = new Node;
+    (*root)->element = num;
+    (*root)->lChild = NULL;
+    (*root)->rChild = NULL;
+
+    cout << "输入" << num << "的左孩子：";
+    CreateBinaryTree(&((*root)->lChild));
+    cout << "输入" << num << "的右孩子：";
+    CreateBinaryTree(&((*root)->rChild));
+}
+
+// 先序遍历
+void PreOrder(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    cout << root->element << " ";
+    PreOrder(root->lChild);
+    PreOrder(root->rChild);
+}
+
+int main(int argc, const char * argv[])
+{
+    Node *root;
+
+    cout << "请输入二叉树的节点，输入-1结束:" << endl;
+    CreateBinaryTree(&root);
+
+    cout << "先序遍历的结果为：";
+    PreOrder(root);
+
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
